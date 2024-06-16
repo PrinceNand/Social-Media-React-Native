723b1655ea8c9ce873c497bd8bf6c96ce04ece86
Prince <ashnand2000@gmail.com>
2024-06-16 23:26:07 +0530

    Add FontAwesome icon and created global style

--- components/Title/Title.js
+++ components/Title/Title.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import style from './style';
+import globalStyle from '../../style/globalStyle';
 import PropTypes from 'prop-types';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 const Title = props => {
   return (
     <SafeAreaView>
-      <Text style={style.title}>{props.title}</Text>
+      <View style={globalStyle.header}>
+        <Text style={style.title}>{props.title}</Text>
+        <TouchableOpacity style={globalStyle.messageMain}>
+          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
+          <View style={globalStyle.messageNumberContainer}>
+            <Text style={globalStyle.messageNumber}>2</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
